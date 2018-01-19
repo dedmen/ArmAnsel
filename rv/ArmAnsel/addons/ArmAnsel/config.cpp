8764a44b971299ef796d798f61da31a65b99b9fa
Dedmen Miller <dedmen@dedmen.de>
2018-01-19 14:50:19 +0100

    push1

---
+++ rv/ArmAnsel/addons/ArmAnsel/config.cpp
@@ -0,0 +1,18 @@
+class CfgPatches {
+    class dedmen_ArmAnsel {
+        units[] = {""};
+        weapons[] = {};
+        requiredVersion = 0.1;
+        requiredAddons[] = { "Intercept_Core"};
+        version = 0.1;
+		author = "dedmen";
+    };
+};
+
+class Intercept {
+    class dedmen {
+        class ArmAnsel {
+            pluginName = "ArmAnsel";
+        };
+    };
+};
---
+++ src/client_dll.cpp
@@ -0,0 +1,25 @@
+#include <intercept.hpp>
+
+int intercept::api_version() {
+    return 1;
+}
+
+void  intercept::on_frame() {
+
+}
+
+void intercept::pre_start() {
+
+}
+
+void intercept::pre_init() {
+
+}
+
+void intercept::post_init() {
+    
+}
+
+void intercept::mission_ended() {
+
+}
